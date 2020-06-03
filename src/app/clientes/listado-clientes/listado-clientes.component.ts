3c0ef1d71913fb41e8869bb3bcf929e44d9ce896
“coreavparis2020m” <coreavparis2020m@gmail.com>
2020-06-03 09:59:18 +0200

    initial

---
+++ src/app/clientes/listado-clientes/listado-clientes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ClientesService } from 'src/app/servicios/clientes.service';
+
+@Component({
+  selector: 'app-listado-clientes',
+  templateUrl: './listado-clientes.component.html',
+  styleUrls: ['./listado-clientes.component.scss']
+})
+export class ListadoClientesComponent implements OnInit {
+
+    clientes: any[];
+
+    constructor(private clientesService: ClientesService) {}
+
+    ngOnInit() {
+       this.clientes = this.clientesService.getClientes();
+    }
+
+}
---
+++ src/app/servicios/clientes.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ClientesService {
+
+    clientes = [
+        {nombre: 'Iberdrola S.A.U.', cif: 'A12345678'},
+        {nombre: 'Telefónica S.A.U.', cif: 'A87654321'},
+        {nombre: 'Gas Natural S.A.U.', cif: 'A56781234'}
+    ]
+
+    constructor() { }
+
+    getClientes() {
+        return this.clientes;
+    }
+
+}
